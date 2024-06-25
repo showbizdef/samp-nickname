052868ea1de3480ba14a9d0c6a8427cd93e3010b
Maxim Sitowskyi <75034428+showbizdef@users.noreply.github.com>
2024-06-25 04:00:41 +0300

    first version

---
+++ script.js
@@ -0,0 +1,14 @@
+const firstNames = ["Amaterasu", "Sora", "Emperor", "Hiroshi", "Amethyst", "Playada", "Adaptive", "Rebellious", "Yamato", "Dagon", "Katsu", "Saint", "Chrome", "Manera", "Arata", "Phantom", "Mamora", "Shirou", "Infused", "Jeffrey", "Shadow", "Neri"];
+const lastNames = ["Edge", "Tatsuki", "Cartier", "Explorer", "Blacksimens", "Cult", "Castle", "Cho", "Northside", "Eternal", "Devilside", "Destruction", "Murasaki", "Violence", "Recovery", "Armano", "Takeda", "Soyama", "Hellwalker"];
+
+function generateNickname() {
+    const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+    return `${randomFirstName}_${randomLastName}`;
+}
+
+
+document.getElementById('generate-btn').addEventListener('click', () => {
+    const nickname = generateNickname();
+    document.getElementById('nickname').innerText = nickname;
+});
